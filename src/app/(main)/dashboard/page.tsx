78fea55f796c6b2877e256cabb7ce4e7dd138e86
Kanzoonrekza <kanzoonrkz@gmail.com>
2024-06-11 09:50:33 +0700

    refine dashboard ui

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -6,34 +6,33 @@ export default function Dashboard() {
 	const templatesData: templateDetailType[] = mockTemplateDetails;
 
 	return (
-		<main>
-			<div className="flex items-center justify-between">
-				<div>Templates</div>
-				<button className="bg-slate-700">Add New Template</button>
+		<main className="p-10 flex flex-col gap-10">
+			<div className="flex items-center justify-between ">
+				<div className="text-4xl">All Templates</div>
+				<Link href={"/templates/add"} className="bg-slate-700 p-2 rounded-md">
+					Add New Template
+				</Link>
 			</div>
 			<div className="grid grid-cols-4 gap-5">
 				{templatesData.map((template) => (
-					<div className="border p-2 rounded-lg">
-						<div className="flex items-center justify-start h-10 gap-4">
-							<div className="aspect-square bg-slate-600 h-full grid place-content-center">
-								G
+					<Link
+						href={`/template/${template.id}`}
+						key={template.id}
+						className="border p-3 rounded-lg"
+					>
+						<div className="flex items-center justify-start h-10 gap-3 mb-4">
+							<div className="aspect-square bg-slate-600 h-full grid place-content-center rounded">
+								T
 							</div>
 							<div className="flex-grow">
-								<Link href={`/template/${template.id}`} key={template.id}>
-									{template.title}
-								</Link>
-								<div>{`${template.category} ${
+								<div>{template.title}</div>
+								<div className="text-sm">{`${template.category} ${
 									template.apiReady ? "- Api Ready" : ""
 								} `}</div>
 							</div>
-							<button className="bg-red-500 h-full px-2">:</button>
 						</div>
-						<p>{template.description}</p>
-						<div className="grid grid-cols-2 gap-2">
-							<button>Edit</button>
-							<button className="bg-slate-500">Generate</button>
-						</div>
-					</div>
+						<p className="line-clamp-3">{template.description}</p>
+					</Link>
 				))}
 			</div>
 		</main>
