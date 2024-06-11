221568afc37a10658332f182ce1a0347e39ca829
Kanzoonrekza <kanzoonrkz@gmail.com>
2024-06-11 10:53:45 +0700

    add swr, integrate dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,40 +1,17 @@
-import { mockTemplateDetails } from "@/mocks";
-import { templateDetailType } from "@/types";
 import Link from "next/link";
+import TemplateList from "./templateList";
 
 export default function Dashboard() {
-	const templatesData: templateDetailType[] = mockTemplateDetails;
-
 	return (
-		<main className="p-10 flex flex-col gap-10">
+		<main className="flex flex-col gap-10 p-10">
 			<div className="flex items-center justify-between ">
 				<div className="text-4xl">All Templates</div>
-				<Link href={"/templates/add"} className="bg-slate-700 p-2 rounded-md">
+				<Link href={"/templates/add"} className="p-2 rounded-md bg-slate-700">
 					Add New Template
 				</Link>
 			</div>
-			<div className="grid grid-cols-4 gap-5">
-				{templatesData.map((template) => (
-					<Link
-						href={`/template/${template.id}`}
-						key={template.id}
-						className="border p-3 rounded-lg"
-					>
-						<div className="flex items-center justify-start h-10 gap-3 mb-4">
-							<div className="aspect-square bg-slate-600 h-full grid place-content-center rounded">
-								T
-							</div>
-							<div className="flex-grow">
-								<div>{template.title}</div>
-								<div className="text-sm">{`${template.category} ${
-									template.apiReady ? "- Api Ready" : ""
-								} `}</div>
-							</div>
-						</div>
-						<p className="line-clamp-3">{template.description}</p>
-					</Link>
-				))}
-			</div>
+			<TemplateList />
+			
 		</main>
 	);
 }
