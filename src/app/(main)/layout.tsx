38680a3c6ab4428fec898a02ad689eb114f01ab7
Kanzoonrekza <kanzoonrkz@gmail.com>
2024-06-25 15:47:52 +0700

    core route to dashboard from header and landing page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function MainLayout({
 	children,
 }: Readonly<{
@@ -5,7 +7,9 @@ export default function MainLayout({
 }>) {
 	return (
 		<div className="h-screen flex flex-col">
-			<div className="sticky top-0 z-10 bg-neutral-800">Docxify</div>
+			<div className="sticky top-0 z-10 bg-neutral-800">
+				<Link href="/dashboard">Docxify</Link>
+			</div>
 			<div className="flex-1 max-w-screen-2xl mx-auto w-full">{children}</div>
 		</div>
 	);
