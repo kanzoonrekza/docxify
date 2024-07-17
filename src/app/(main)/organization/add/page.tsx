63843a87f3b1010b5bcabe7b6e188913493eabea
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-18 00:16:48 +0700

    apply style to create organization

--- src/app/(main)/organization/add/page.tsx
+++ src/app/(main)/organization/add/page.tsx
@@ -3,11 +3,9 @@ import FormOrganization from "./formOrganization";
 
 export default function AddOrganization() {
 	return (
-		<main className="flex flex-col gap-5 p-10 h-full">
-			<h1 className="text-5xl font-medium mb-5">Add New Organization</h1>
-			<div className="gap-5 h-full mx-auto max-w-screen-sm w-full">
-				<FormOrganization />
-			</div>
+		<main className="flex flex-col gap-10 p-10">
+			<h1 className="text-3xl font-bold">Create New Organization</h1>
+			<FormOrganization />
 		</main>
 	);
 }
