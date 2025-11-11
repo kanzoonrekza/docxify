919c9ac0992c4628ac0691e4418b604492bb3c2d
kanzoonrekza <kanzoonrkz@gmail.com>
2025-11-11 22:19:54 +0700

    nyoh

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,10 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  database: process.env.POSTGRES_DB!,
+  user: process.env.POSTGRES_USER!,
+  password: process.env.POSTGRES_PASSWORD!,
+  host: process.env.POSTGRES_HOST!,
 });
 
 export const db = drizzle(pool);
