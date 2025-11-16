268d222df6d04104b9740d1c2e0328c377ec9e95
kanzoonrekza <kanzoonrkz@gmail.com>
2025-11-16 13:49:39 +0700

    merge user schema and make the username unique on non deleted

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,26 +1,26 @@
+import { isNull } from "drizzle-orm";
 import {
   integer,
   pgTable,
   serial,
   text,
   timestamp,
+  uniqueIndex,
   varchar,
 } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: serial().notNull().primaryKey().unique(),
-  username: varchar().notNull().unique(),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  updatedAt: timestamp({ withTimezone: true }).defaultNow(),
-  archivedAt: timestamp({ withTimezone: true }),
-});
-
-export const userProfiles = pgTable("user_profiles", {
-  userId: integer()
-    .notNull()
-    .primaryKey()
-    .references(() => users.id),
-  email: varchar().notNull().unique(),
-  fullName: text(),
-  referal: varchar(),
-});
+export const users = pgTable(
+  "users",
+  {
+    id: serial().notNull().primaryKey().unique(),
+    username: varchar().notNull(),
+    email: varchar().notNull(),
+    fullName: text(),
+    referral: varchar(),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).defaultNow(),
+    archivedAt: timestamp({ withTimezone: true }),
+    deletedAt: timestamp({ withTimezone: true }),
+  },
+  (users) => [uniqueIndex().on(users.username).where(isNull(users.deletedAt))]
+);
