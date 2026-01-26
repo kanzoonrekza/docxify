c8aab2fb5e7d68812ee153d2745ea3e87a085c3d
kanzoonrekza <kanzoonrkz@gmail.com>
2026-01-26 20:43:37 +0700

    proper unbiased mock data

---
+++ src/app/api/mock/ol/route.ts
@@ -0,0 +1,44 @@
+import olData from "@/mocks/file/olExample/OLData.json";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+	const mockdata = await request.nextUrl.searchParams.get("data");
+	const rawData = olData;
+
+	if (!mockdata) {
+		return NextResponse.json(
+			{
+				status: 400,
+				message: "Search parameters missing: 'data'",
+			},
+			{ status: 400 }
+		);
+	}
+
+	if (!rawData) {
+		return NextResponse.json(
+			{
+				status: 400,
+				message: "Error retrieving data",
+			},
+			{ status: 400 }
+		);
+	}
+
+	const selectedData = rawData[mockdata];
+
+	if (!selectedData) {
+		return NextResponse.json(
+			{
+				status: 400,
+				message: `Data not found for data ${mockdata}. Available data: ${Object.keys(rawData).join(", ")}`,
+			},
+			{ status: 400 }
+		);
+	}
+
+	return NextResponse.json({
+		status: 200,
+		data: selectedData,
+	});
+}
